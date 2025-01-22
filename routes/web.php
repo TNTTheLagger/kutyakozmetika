66ea693fa0d70c489374d842a7ffc05e7be5daf6
Hazel <37238787+TNTTheLagger@users.noreply.github.com>
2025-01-22 10:34:35 +0100

    Alright server works!

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('app');
-})->name('app');
+    return Inertia::render('main');
+})->name('main');
 
 
 //Route::get('/', function () {
