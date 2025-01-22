7d6fc5278dcf23cb95ad1676a080c9717b1b2561
Hazel <37238787+TNTTheLagger@users.noreply.github.com>
2025-01-22 10:20:25 +0100

    Set up project ready for development

---
+++ resources/js/Pages/app.vue
@@ -0,0 +1,6 @@
+<script>
+</script>
+
+<template>
+    <p>Ello</p>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-
+Route::get('/', function () {
+    return Inertia::render('app');
+})->name('app');
 
 
 //Route::get('/', function () {
